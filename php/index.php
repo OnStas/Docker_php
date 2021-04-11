b152ff1ccc45ba65d1906c84302617e957fa2212
OnStas <stanislav-helios@i.ua>
2021-04-11 21:02:00 +0300

    Update index.php

--- php/index.php
+++ php/index.php
@@ -2,17 +2,6 @@
 
 echo 'hello php2mysql';
 
-$host = 'db'; //servise name from docker-compose.yml
-$user = 'devuser';
-$password = 'devpass';
-$db = 'test_db';
-
-$conn = new mysql($host,$user,$password,$db);
-if($conn->connect_error){
-	echo 'connection failed' . $conn->connect_error;
-}
-echo 'Successfully connected to MYSQL';
-
 
 ?>
 
